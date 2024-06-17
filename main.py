abe29003ffc7c80bac46c0263cf6bea3f8870f7e
Kirill <matytsyn7@gmail.com>
2024-06-17 23:27:37 +0300

    Sped up the algorithm

--- main.py
+++ main.py
@@ -1,30 +1,9 @@
-def is_valid(board, row, col, num):
-    for x in range(9):
-        if board[row][x] == num:
-            return False
-
-    for x in range(9):
-        if board[x][col] == num:
-            return False
-
-    start_row = row - row % 3
-    start_col = col - col % 3
-    for i in range(3):
-        for j in range(3):
-            if board[i + start_row][j + start_col] == num:
-                return False
+def is_valid(board, row, col, num, row_set, col_set, subgrid_set):
+    if num in row_set[row] or num in col_set[col] or num in subgrid_set[row // 3][col // 3]:
+        return False
     return True
 
-
-def find_empty(board):
-    for i in range(9):
-        for j in range(9):
-            if board[i][j] == 0:
-                return i, j
-    return None
-
-
-def solve_sudoku(board):
+def solve_sudoku(board, row_set, col_set, subgrid_set):
     find = find_empty(board)
     if not find:
         return True
@@ -32,16 +11,28 @@ def solve_sudoku(board):
         row, col = find
 
     for i in range(1, 10):
-        if is_valid(board, row, col, i):
+        if is_valid(board, row, col, i, row_set, col_set, subgrid_set):
             board[row][col] = i
+            row_set[row].add(i)
+            col_set[col].add(i)
+            subgrid_set[row // 3][col // 3].add(i)
 
-            if solve_sudoku(board):
+            if solve_sudoku(board, row_set, col_set, subgrid_set):
                 return True
 
             board[row][col] = 0
+            row_set[row].remove(i)
+            col_set[col].remove(i)
+            subgrid_set[row // 3][col // 3].remove(i)
 
     return False
 
+def find_empty(board):
+    for i in range(9):
+        for j in range(9):
+            if board[i][j] == 0:
+                return i, j
+    return None
 
 def print_board(board):
     for i in range(9):
@@ -57,8 +48,7 @@ def print_board(board):
             else:
                 print(str(board[i][j]) + " ", end="")
 
-
-# Инициализация матрицы Судоку
+# Initialize the Sudoku board
 board = [
     [5, 3, 4, 0, 7, 0, 0, 0, 0],
     [6, 0, 0, 1, 9, 5, 0, 0, 0],
@@ -71,7 +61,19 @@ board = [
     [0, 0, 0, 0, 8, 0, 0, 7, 9]
 ]
 
-if solve_sudoku(board):
+row_set = [set() for _ in range(9)]
+col_set = [set() for _ in range(9)]
+subgrid_set = [[set() for _ in range(3)] for _ in range(3)]
+
+for i in range(9):
+    for j in range(9):
+        if board[i][j] != 0:
+            num = board[i][j]
+            row_set[i].add(num)
+            col_set[j].add(num)
+            subgrid_set[i // 3][j // 3].add(num)
+
+if solve_sudoku(board, row_set, col_set, subgrid_set):
     print_board(board)
 else:
     print("No solution exists")
